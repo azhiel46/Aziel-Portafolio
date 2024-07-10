fa8af332669f7c4ba17aa9666ba048f39d3316b9
azhiel46 <aziel_balocamp56@hotmail.com>
2024-07-10 16:01:24 -0600

    Commit Prueba Credenciales

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 //Importar Componentes
-import Intro from './Components/intro/Intro';
-import About from './Components/About/About';
-import ProductList from './Components/Products/ProductList';
-import Contact from './Components/Contact/Contact';
-import Toggle from './Components/Toggle/Toggle';
-import './App.css';
-import { useContext } from 'react';
-import { ThemeContext } from './Context';
+import Intro from "./Components/intro/Intro";
+import About from "./Components/About/About";
+import ProductList from "./Components/Products/ProductList";
+import Contact from "./Components/Contact/Contact";
+import Toggle from "./Components/Toggle/Toggle";
+import "./App.css";
+import { useContext } from "react";
+import { ThemeContext } from "./Context";
 
 function App() {
-
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
   return (
-    <div style={{ backgroundColor:darkMode ? '#222' : 'white', color:darkMode && 'white' }}>
-        <Toggle />
-        <Intro />
-        <About/>
-        <ProductList />
-        <Contact/>
+    <div
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode && "white",
+      }}
+    >
+      <Toggle />
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
     </div>
   );
 }
-
 export default App;
